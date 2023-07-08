c1ffb4d79baea4eec7c0b209da5615a0a19e7c14
sagadavid <epostsaga@gmail.com>
2023-07-08 09:20:07 +0200

    moongose pack updated, indexjscode fixed vchatgpt

--- index.js
+++ index.js
@@ -3,40 +3,47 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const mongoose = require("mongoose");
-const url = "mongodb://localhost/blog"; //url of data at mongoDB
+const url = "mongodb://localhost/blog"; // URL of the MongoDB database
 const User = require("./user");
 
 mongoose.set("strictQuery", true);
 
-app.use(bodyParser.json()); //parse coming request
+app.use(bodyParser.json()); // Parse incoming requests
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//at the end, send matched/found user to login component via url
-app.post("/api/user/login", (req, res) => {
-  //open monoose default connection to mongoDB
-  mongoose.connect(url, {}, function (err) {
-    if (err) throw err;
-    //pars and match monoose user in the request and respond
-    User.find(
-      {
-        username: req.body.username,
-        password: req.body.password,
-      },
-      function (err, user) {
-        if (err) throw err;
-        if (user.length === 1) {
-          return res.status(200).json({
-            status: "success",
-            data: user,
-          });
-        } else {
-          return res.status(200).json({
-            status: "fail",
-            message: "Login Failed",
-          });
-        }
-      }
-    );
-  });
+// Endpoint for user login
+app.post("/api/user/login", async (req, res) => {
+  try {
+    // Open a connection to the MongoDB database
+    await mongoose.connect(url);
+
+    // Find the user in the database
+    const user = await User.find({
+      username: req.body.username,
+      password: req.body.password,
+    });
+
+    if (user.length === 1) {
+      return res.status(200).json({
+        status: "success",
+        data: user,
+      });
+    } else {
+      return res.status(200).json({
+        status: "fail",
+        message: "Login Failed",
+      });
+    }
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    return res.status(500).json({
+      status: "error",
+      message: "Internal server error",
+    });
+  } finally {
+    // Close the MongoDB connection
+    mongoose.connection.close();
+  }
 });
-app.listen(3000, () => console.log("hører på porten 3000"));
+
+app.listen(3000, () => console.log("Listening on port 3000"));
